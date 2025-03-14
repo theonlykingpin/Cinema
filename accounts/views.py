6cc164ca93a7e9251e1edc42e9b1a01611bfdb16
Reza Rohani <theonlykingpin@outlook.com>
2025-03-14 09:39:27 +0330

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,48 +1,42 @@
 from django.contrib.auth import logout, authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
-
 from accounts.forms import PaymentForm, ProfileForm, MyUserForm
 from accounts.models import Payment
 
-
 def login_view(request):
     next_url = request.GET.get('next')
+    context = {}
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            # Successful login
             login(request, user)
             redirect_url = next_url if next_url else reverse('ticketing:showtime_list')
-            return HttpResponseRedirect(redirect_url)
+            return redirect(redirect_url)
         else:
-            # undefined user or wrong password
             context = {
                 'username': username,
                 'error': 'کاربری با این مشخصات یافت نشد'
             }
-    else:
-        context = {}
     return render(request, 'accounts/login.html', context)
-
 
 def logout_view(request):
     logout(request)
-    return HttpResponseRedirect(reverse('accounts:login'))
-
+    return redirect('accounts:login')
 
 @login_required
 def profile_details(request):
-    profile = request.user.profile
-    context = {
-        'profile': profile
-    }
-    return render(request, 'accounts/profile_details.html', context)
-
+    if hasattr(request.user, 'profile'):
+        profile = request.user.profile
+        context = {
+            'profile': profile
+        }
+        return render(request, 'accounts/profile_details.html', context)
+    else:
+        return redirect('accounts:profile_edit')
 
 @login_required
 def payment_list(request):
@@ -51,7 +45,6 @@ def payment_list(request):
         'payments': payments
     }
     return render(request, 'accounts/payment_list.html', context)
-
 
 @login_required
 def payment_create(request):
@@ -62,14 +55,13 @@ def payment_create(request):
             payment.profile = request.user.profile
             payment.save()
             request.user.profile.deposit(payment.amount)
-            return HttpResponseRedirect(reverse('accounts:payment_list'))
+            return redirect('accounts:payment_list')
     else:
         payment_form = PaymentForm()
     context = {
         'payment_form': payment_form
     }
     return render(request, 'accounts/payment_create.html', context)
-
 
 @login_required
 def profile_edit(request):
@@ -79,7 +71,7 @@ def profile_edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-            return HttpResponseRedirect(reverse('accounts:profile_details'))
+            return redirect('accounts:profile_details')
     else:
         user_form = MyUserForm(instance=request.user)
         profile_form = ProfileForm(instance=request.user.profile)
